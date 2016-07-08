9097a16a6c35c3e516d387f1e5063ccb46197dcf
jplamb <j.lamb13@gmail.com>
2016-07-07 21:03:12 -0400

    Lesson 2

--- Lesson2Clique.py
+++ Lesson2Clique.py
@@ -3,11 +3,23 @@
 
 # How many edges in a complete graph on n nodes?
 
+# my initial solution
+# (n-1)!
 def clique(n):
 	m = 0
 	while n > 0:
 		m += n-1
 		n -= 1
 	return m
-	
-print clique(5)
+
+# Provided solution
+# n*(n-1)/2
+def cliqueAns(n):
+	return n*(n-1)/2
+
+# Test case
+for x in range(1000):
+	if clique(x) <> cliqueAns(x):
+		print 'False: ', clique(x), ' ', cliqueAns(x)
+		
+# interesting that these two algorithms both work despite have different mathematic formulae
